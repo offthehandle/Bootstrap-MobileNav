89559827af7de7eff9a89d8ff2eb11c1862096fc
offthehandle <offthehandle@users.noreply.github.com>
2018-05-08 00:44:00 -0400

    Completed Mobile Nav 2 beta release

--- js/bootstrap-mobile-nav.js
+++ js/bootstrap-mobile-nav.js
@@ -1,74 +1,94 @@
 ﻿
 /*
- * Mobile Nav Plugin for Twitter Bootstrap 3
+ * Mobile Nav with support for Twitter Bootstrap 3 & 4
  * 
- * Copyright (c) 2015 Adam J De Lucia
+ * Description: Mobile Nav is a Bootstrap extension that provides a better, fat finger free UX in the mobile state.
+ * 
+ * Copyright (c) 2015 - 2018 Adam J. De Lucia
  * Licensed under the MIT License.
  * http://opensource.org/licenses/MIT
  * 
- * Author: Adam J De Lucia
- * Version: 1.2.0
- * Date: November 8, 2015
- * 
+ * Author: Adam J. De Lucia
+ * Version: 2.0.0-beta
+ * Date: May 8, 2018
  */
 
 (function ($) {
     $.fn.mobileNav = function (options) {
+
         this.each(function () {
+
             var settings = $.extend({
-                collapsingNavEl: '.navbar-collapse',
-                mobileNavLink: '.nav-btn',
-                hideMobileNavBtnParent: '#nav-btn-out',
-                hideMobileNavBtn: 'a',
-                navbarToggleHidden: 768,
-                hideBackdropOn: 'hidden',
-                showBackdropOn: 'shown',
-                hideMobileNavOnNavBtnClick: false,
-                hideMobileNavOnResizeDelay: 250
+                collapse: '.navbar-collapse',
+                backdropIn: 'shown',
+                backdropOut: 'hidden',
+                closeId: '#mn-close',
+                closeBtn: 'a',
+                minWidthHidden: 768,
+                resizeDelay: 250,
+                hideOnClick: false,
+                onClickLink: '.mn-link'
             }, options);
 
-            // Handles hide mobile nav button click
-            $(settings.hideMobileNavBtnParent).on('click', settings.hideMobileNavBtn, function (e) {
+
+            // Handles mobile nav close
+            $(settings.closeId).on('click', settings.closeBtn, function (e) {
                 e.preventDefault();
 
-                // Protects hide event from firing on resize when nav is already collapsed
-                // Bootstrap will trigger shown.bs.collapse event on hide when hide is called on a collapsed nav
-                if ($(settings.collapsingNavEl).hasClass('in')) {
-                    $(settings.collapsingNavEl).collapse('hide');
+                // Only triggers close if mobile nav is open
+                if ($(settings.collapse).hasClass('in') || $(settings.collapse).hasClass('show')) {
+
+                    $(settings.collapse).collapse('hide');
                 }
             });
 
-            // Dismisses mobile nav on nav button click
-            // Handles same-page links in mobile nav
-            if (settings.hideMobileNavOnNavBtnClick) {
-                $(settings.collapsingNavEl).on('click', settings.mobileNavLink, function () {
-                    $(settings.hideMobileNavBtnParent).find(settings.hideMobileNavBtn).trigger('click');
+
+            // Closes the nav when same-page bookmarks are clicked
+            if (settings.hideOnClick) {
+
+                $(settings.collapse).on('click', settings.onClickLink, function () {
+
+                    $(settings.closeId).find(settings.closeBtn).trigger('click');
                 });
             }
 
-            // Appends backdrop on mobile nav show/shown event
-            $(settings.collapsingNavEl).on(settings.showBackdropOn + '.bs.collapse', function () {
+
+            // Adds backdrop between menu and site content on backdropIn
+            $(settings.collapse).on(settings.backdropIn + '.bs.collapse', function () {
+
                 $('<div class="modal-backdrop"></div>').appendTo($('body'));
             });
 
-            // Removes backdrop on mobile nav hide/hidden event
-            $(settings.collapsingNavEl).on(settings.hideBackdropOn + '.bs.collapse', function () {
+            // Removes backdrop on backdropOut
+            $(settings.collapse).on(settings.backdropOut + '.bs.collapse', function () {
+
                 $('.modal-backdrop').remove();
             });
 
-            // Optimizes resize event performance
+
+            // Optimizes performance on resize
+            var resizeTimer = null;
+
             $(window).resize(function () {
-                clearTimeout(resizeTimer);
-                var resizeTimer = setTimeout(hideMobileNav, settings.hideMobileNavOnResizeDelay);
+
+                if (resizeTimer !== null) {
+
+                    clearTimeout(resizeTimer);
+                }
+
+                resizeTimer = setTimeout(onResize, settings.resizeDelay);
             });
 
-            // Optimizes mobile nav performance
-            // Used to automatically hide mobile nav when moving from mobile to desktop state
-            function hideMobileNav() {
-                if ($(window).width() >= settings.navbarToggleHidden) {
-                    $(settings.hideMobileNavBtnParent).find(settings.hideMobileNavBtn).trigger('click');
+
+            // Hides within given max-width on resize
+            function onResize() {
+
+                if (window.innerWidth >= settings.minWidthHidden) {
+
+                    $(settings.closeId).find(settings.closeBtn).trigger('click');
                 }
             }
         });
     };
+
 }(jQuery));
